e642884cb16f391ca8a380dd0e2117250f8cfafb
jace.lee <jace.lee@doubleme.me>
2025-05-30 14:27:00 +0900

    Init

---
+++ Assets/Scripts/TestA.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameStudio.Common
+{
+
+    /// <summary>
+    /// 테스트 클래스입니다.
+    /// </summary>
+    public class TestA : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
